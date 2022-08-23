0ee34a16700396dc9b4bfa861b3d07d32bade68e
Pandaphone <shawngreene075@gmail.com>
2022-08-22 21:22:24 -0400

    Saving what I have.

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,23 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <div className='tophalf'>
+            <img src={data.place.pic} alt="" />
+            <div className='sidebar'>
+            <h1>{ data.place.name }</h1>
+            <h2>Rating</h2>
+            <p>Not Rated.</p>
+            <h2>Description</h2>
+            <p>Located in {data.place.city}, {data.place.state}</p>
+            
+            <h2>Comments</h2>
+            <p>No comments yet.</p>
+            </div>
+            </div>
           </main>
         </Def>
     )
