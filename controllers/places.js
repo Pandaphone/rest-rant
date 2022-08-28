4e14aadedf00db0016a4e380c88842ffc931d881
Pandaphone <shawngreene075@gmail.com>
2022-08-27 23:56:29 -0400

    Part 6 done

--- controllers/places.js
+++ controllers/places.js
@@ -18,7 +18,7 @@ router.get('/new', (req, res) => {
       res.render('error404')
     }
     else {
-      res.render('places/show', { place: places[id] })
+      res.render('places/show', { place: places[id], id })
     }
   })
 
@@ -37,5 +37,18 @@ router.get('/new', (req, res) => {
     res.redirect('/places')
   })
   
+  router.delete('/places/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
 
 module.exports = router
