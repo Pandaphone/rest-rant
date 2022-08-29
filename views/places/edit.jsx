accd822271bc41b832063f48f8ca44e5a6b37048
Pandaphone <shawngreene075@gmail.com>
2022-08-29 12:34:35 -0400

    Final commit. Done with part 11. Time for some cleanup.

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -30,6 +30,14 @@ function edit_form (data) {
                 <div className="form-group">
                     <label htmlFor="cuisines">Cuisines</label>
                     <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+                    <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded</label>
+            <input className="form-control" 
+              id="founded" 
+              name="founded" 
+              value={data.place.founded} 
+            />
+        </div>
                 </div>
                     <input type="hidden" name="_method" value="DELETE"/>
                     <input className="btn btn-primary" type="submit" value="Update Place" />
