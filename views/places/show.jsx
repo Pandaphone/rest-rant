4ea751f574041d35ee5bc69e0fdfff241ec3c798
Pandaphone <shawngreene075@gmail.com>
2022-08-28 20:00:10 -0400

    Done with Part 9!

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,26 +4,37 @@ const Def = require('../default')
 function show (data) {
     return (
         <Def>
-          <main>
-            <div className='tophalf'>
-            <img src={data.place.pic} alt="" />
-            <div className='sidebar'>
-            <h1>{ data.place.name }</h1>
-            <h2>Rating</h2>
-            <p>Not Rated.</p>
-            <h2>Description</h2>
-            <p>Located in {data.place.city}, {data.place.state}</p>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-              Edit
-            </a>      
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-              <button type="submit" className="btn btn-danger">
-                Delete
-              </button>
-            </form>       
-            <h2>Comments</h2>
-            <p>No comments yet.</p>
-            </div>
+          <main className='container'>
+            <div className='row'>
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+
+              <div className='col-sm-6'>
+                <h1>{ data.place.name }</h1>
+                <h2>Rating</h2>
+                <p>Not Rated.</p>
+                <h2>Description</h2>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                  Edit
+                </a>      
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                  <button type="submit" className="btn btn-danger">
+                    Delete
+                  </button>
+                </form>       
+                <h2>Comments</h2>
+                <p>No comments yet.</p>
+              </div>
             </div>
           </main>
         </Def>
